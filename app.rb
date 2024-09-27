1eb5fc17c1a71de08b3620b88ed78b8dd5ec22e8
vitor <vitaucruz@gmail.com>
2024-09-27 12:58:03 -0300

    adicionando perguntas

--- app.rb
+++ app.rb
@@ -1,7 +1,9 @@
 require 'C:/Users/Vhenr/OneDrive/Área de Trabalho/DesafiosRuby-DIO/Dinamica_mercado/product.rb'
 require 'C:/Users/Vhenr/OneDrive/Área de Trabalho/DesafiosRuby-DIO/Dinamica_mercado/market.rb'
 
+puts "Qual produto você deseja?"
 produto_nome = gets.chomp
+puts "Qual o valor?"
 produto_preco=gets.chomp.to_f
 
 produto = Product.new(produto_nome,produto_preco)
